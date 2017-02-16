053fa476a964a80be78f2587c82ca61eae2680ff
Blake Tsuzaki <dtsuzaki@scu.edu>
2017-02-15 20:03:08 -0800

    Added Cloud Code Push Function

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,20 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('sendPush', function(req, res) {
+    var query = new Parse.Query(Parse.Installation);
+    query.exists("deviceToken");
+    var payload = { alert: "Meow" };
+
+    Parse.Push.send({
+        data: payload,
+        where: query
+    }, { useMasterKey: true })
+
+    .then(function() {
+        res.success("Push Sent!");
+    }, function(error) {
+        res.error(error.message);
+    });
+})
